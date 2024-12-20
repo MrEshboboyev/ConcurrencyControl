7a0d18ddfd775fe3445bec3481d4f28fe0a908a7
mreshboboyev <mreshboboyev@gmail.com>
2024-12-20 14:10:48 +0500

    Bank Account Repo interface and implementation

---
+++ ConcurrencyControl.Domain/Abstracts/Persistence/Repositories/IBankAccountRepository.cs
@@ -0,0 +1,9 @@
+﻿using ConcurrencyControl.Domain.Entities;
+
+namespace ConcurrencyControl.Domain.Abstracts.Persistence.Repositories;
+
+public interface IBankAccountRepository
+{
+    Task CreateBankAccountAsync(string accountNumber, string ownerName);
+    Task CreateBankAccountAsync(Guid accountId, decimal amount);
+}
---
+++ ConcurrencyControl.Persistence/Repositories/BankAccountRepository.cs
@@ -0,0 +1,42 @@
+﻿using ConcurrencyControl.Domain.Abstracts.Persistence.Repositories;
+using ConcurrencyControl.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConcurrencyControl.Persistence.Repositories;
+
+public class BankAccountRepository(DemoDbContext context)
+    : IBankAccountRepository
+{
+    private readonly DemoDbContext _context = context;
+
+    public async Task CreateBankAccountAsync(string accountNumber, string ownerName)
+    {
+        await _context.BankAccounts.AddAsync(new BankAccount()
+        {
+            AccountNumber = accountNumber,
+            OwnerName = ownerName
+        });
+        
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task CreateBankAccountAsync(Guid accountId, decimal amount)
+    {
+        var account = await _context.BankAccounts.FindAsync(accountId)
+            ?? throw new ArgumentException("Account not found");
+        
+        account.Balance += amount;
+
+        try
+        {
+
+        }
+        catch (DbUpdateConcurrencyException ex)
+        {
+            await ex.Entries.Single().ReloadAsync();
+            Console.WriteLine($"Concurrency exception occured: {ex.Message}");
+            throw;
+        }
+        
+    }
+}
